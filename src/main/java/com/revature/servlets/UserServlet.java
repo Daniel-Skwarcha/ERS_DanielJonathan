1ade109f137b4aeba4da2278450c3de852ecc68c
Jay Norman <jonathan.norman.park.alumni@gmail.com>
2021-02-24 16:06:40 -0600

    Added BufferedReader objects to servlets

--- src/main/java/com/revature/servlets/UserServlet.java
+++ src/main/java/com/revature/servlets/UserServlet.java
@@ -4,26 +4,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+    private BufferedReader reader;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        reader = req.getReader();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        reader = req.getReader();
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        reader = req.getReader();
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        reader = req.getReader();
     }
 }
